08e4ad75ce49ef53418a8e2bade3ef758d35e0de
aninha <115225277+anocao@users.noreply.github.com>
2023-06-11 15:17:44 -0300

    Add files via upload

--- js/myScript.js
+++ js/myScript.js
@@ -1,49 +1,61 @@
-function form() {
-    try {
 
-      if (!nome.value || !id.value || !tipoCliente.value || !cep.value || !rua.value || !numero.value || !bairro.value || !pontoReferencia.value || !cidade.value || !dataNascimento.value || !vendedor.value || !limiteCredito.value) {
-        throw "Todos os campos devem ser preenchidos";
-      }
+const form = document.getElementById("form");
+const username = document.getElementById("username")
+const endereco = document.getElementById("endereco")
+const cep = document.getElementById("cep")
+const bairro = document.getElementById("bairro")
+const dataNascimento = document.getElementById("dataNascimento")
+const vendedor = document.getElementById("vendedor")
+const limiteCredito = document.getElementById("limiteCredito")
+;
 
-      if (!/^\d+$/.test(id.value)) {
-        throw "ID inválido. Deve conter apenas números";
-      }
+function username() {
+if (!username.value || !id.value || !endereco.value || !cep.value || !bairro.value || !dataNascimento.value || !vendedor.value || !limiteCredito.value) {
+  throw "Todos os campos devem ser preenchidos";
+}
 
-      if (!/^\d{5}-\d{3}$/.test(cep.value)) {
-        throw "CEP inválido. Formato esperado: 12345-678";
-      }
+if (!/^[a-zA-Z]+$/.test(username.value)) {
+  throw "Digite um nome válido";
+}
+}
 
-      if (!/^[a-zA-Z]+$/.test(nome.value)) {
-        throw "Nome inválido. Deve conter apenas letras";
-      }
+function id(){
 
-      if (!/^[a-zA-Z]+$/.test(cidade.value)) {
-        throw "Cidade inválida. Deve conter apenas letras";
-      }
+  if (!/^\d+$/.test(id.value)) {
+    throw "Digite apenas números";
+  }
 
-      if (!/^\d+$/.test(limiteCredito.value)) {
-        throw "Limite de crédito inválido. Deve conter apenas números";
-      }
+}
 
-      if (!/^[a-zA-Z]+$/.test(vendedor.value)) {
-        throw "Nome do vendedor inválido. Deve conter apenas letras";
-      }
+function cep(){
+  if (!/^\d{5}-\d{3}$/.test(cep.value)) {
+    throw "CEP inválido";
+  }
+}
+
+function bairro(){
+  if (!/^[a-zA-Z]+$/.test(bairro.value)) {
+    throw "Digite apenas letras";
+  }
+}
+
+function vendedor(){
+
+  if (!/^\d+$/.test(id.value)) {
+    throw "Digite apenas letras";
+  }
+
+}
+
       
-      document.cookie = "nome=" + nome.value;
+      document.cookie = "username=" + username.value;
       document.cookie = "id=" + id.value;
       document.cookie = "cep=" + cep.value;
-      document.cookie = "rua=" + rua.value;
-      document.cookie = "numero=" + numero.value;
+      document.cookie = "endereco=" + endereco.value;
       document.cookie = "bairro=" + bairro.value;
-      document.cookie = "pontoReferencia=" + pontoReferencia.value;
-      document.cookie = "cidade=" + cidade.value;
+      document.cookie = "dataNascimento=" + dataNascimento.value;
       document.cookie = "vendedor=" + vendedor.value;
       document.cookie = "limiteCredito=" + limiteCredito.value;
 
-      alert("Cadastro concluído!");
-    } catch (error) {
-      var errorContainer = document.getElementById("errorContainer");
-      errorContainer.innerHTML = error;
-      errorContainer.style.display = "block";
-    }
-  }
+      alert("Cadastro concluído!"); 
+    
